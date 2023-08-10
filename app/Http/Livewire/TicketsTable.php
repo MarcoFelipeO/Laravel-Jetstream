e09928e24e46220dfc4fe4d230b1bb6198012245
root <root@1PC10O101P22.uautonoma.local>
2023-08-10 09:42:58 -0400

    first commit

--- app/Http/Livewire/TicketsTable.php
+++ app/Http/Livewire/TicketsTable.php
@@ -6,6 +6,12 @@ use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Ticket;
 
+//IMPORTAMOS ESTO PARA LOS BOTONES DE EDITAR Y ELIMINAR
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\Views\Filters\DateFilter;
+//IMPORTAMOS ESTO PARA LOS BOTONES DE EDITAR Y ELIMINAR
+
+
 class TicketsTable extends DataTableComponent
 {
     protected $model = Ticket::class;
@@ -15,6 +21,12 @@ class TicketsTable extends DataTableComponent
         $this->setPrimaryKey('id');
     }
 
+
+        //TIEMPO DE ESPERA ENTRE CADA INSERCION QUE HAGA BUSQUEDAS EN LA BDD
+        public ?int $searchFilterDebounce = 600;
+        //TIEMPO DE ESPERA ENTRE CADA INSERCION QUE HAGA BUSQUEDAS EN LA BDD
+
+
     public function columns(): array
     {
         return [
@@ -22,19 +34,63 @@ class TicketsTable extends DataTableComponent
                 ->sortable(),
 
 
-            Column::make("Title", "title")
+            Column::make("Titulo", "title")
             ->sortable()    
             ->searchable(),
 
-            Column::make("Description", "description")
+            Column::make("Descripción", "description")
             ->sortable()
             ->searchable(),
 
 
-            Column::make("Created at", "created_at")
-                ->sortable(),
-            Column::make("Updated at", "updated_at")
-                ->sortable(),
+            Column::make("Usuarios", "user.name")
+             ->sortable(function($builder, $direction){
+                return $builder->orderBy('users.name', $direction);
+             })
+             ->searchable(),
+            
+
+
+            Column::make("Fecha Creación", "created_at")
+                ->sortable()
+                ->format(function ($value) {
+                    return $value->format('d/m/Y h:i');
+                }),
+
+            Column::make("Fecha Actualización", "updated_at")
+                ->sortable()
+                ->format(function ($value) {
+                    return $value->format('d/m/Y h:i');
+                }),
+
+            Column::make("Acciones", "id")
+                ->format(function ($value) {
+                    return view('tickets.datatables.actions', ['ticket' => Ticket::find($value)]);
+                }, false),
+
+
         ];
     }
+
+
+    public function filters(): array
+    {
+        return [
+
+            DateFilter::make('Edit From')
+                ->config([
+                    'min' => '2023-01-01',
+                    'max' => '2024-01-01',
+                ])
+                ->filter(function(Builder $builder, string $value) {
+                    $builder->where('tickets.updated_at', '>=', $value);
+                }),
+            DateFilter::make('Edit To')
+                ->filter(function(Builder $builder, string $value) {
+                    $builder->where('tickets.updated_at', '<=', $value);
+                }),
+        ];
+    }
+ 
+
 }
--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout> 
-    <div class="p-10 text-center"> 
+    <div class="p-10 "> 
         <livewire:tickets-table /> 
     </div> 
 </x-app-layout> 
